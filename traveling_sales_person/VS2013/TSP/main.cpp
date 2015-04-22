bb59cd0b51c3cb14f330040cec3c1d854b28d2bd
ian-maines <ian@maines.us>
2015-04-21 21:48:54 -0400

    VS2013 project can read in files.

--- traveling_sales_person/VS2013/TSP/main.cpp
+++ traveling_sales_person/VS2013/TSP/main.cpp
@@ -11,9 +11,9 @@
 #include <chrono>
 #include <thread>
 
-#include <iostream>
 #include <fstream>
 #include <string>
+#include <iterator>
 
 
 // My headers.
@@ -21,9 +21,13 @@
 #include "point.h"
 #include "tour.h"
 
+using std::cout;
+using std::endl;
+
 namespace
 {
 using namespace std;
+// Print out program-related data.
 void preamble ()
     {
     cout << "***************************************" << endl
@@ -32,6 +36,20 @@ void preamble ()
          << "***************************************" << endl;
     return;
     }
+
+// Takes a line as they appear in the data files (123.45 67.890) and parses
+// into two doubles
+point_t parse_line (const std::string& strLine)
+	{
+	cout << "parse_line ()" << endl;
+	std::string strTemp (strLine.begin (), strLine.begin () + strLine.find_first_of (" "));
+	double x;
+	x = stod (strTemp);
+	strTemp = std::string (strLine.begin () + strLine.find_first_of (" "), strLine.end ());
+	point_t point (x, stod(strTemp));
+	cout << "[parse_line () created: " << to_string (point) << endl;
+	return point;
+	}
 }
 
 int main(int argc, char* argv[])
@@ -48,11 +66,20 @@ int main(int argc, char* argv[])
 	std::string strLine;
 	std::getline (cDataFile, strLine);
 
+	// Create a 'tour' of all points.
+	tour_t cStartData;
+
 	// Now populate the structure of points.
+	while (std::getline (cDataFile, strLine) &&  !strLine.empty ())
+		{
+		cStartData.push_front (parse_line (strLine));
+		}
 
+	cout << to_string (cStartData) << endl;
 
-
-    std::this_thread::sleep_for (std::chrono::milliseconds (5000));
+	// Exit message
+	cout << endl << "Press ENTER to exit...";
+	std::cin.ignore (std::numeric_limits<std::streamsize>::max (), '\n');
     return 0;
     }
 
--- traveling_sales_person/VS2013/TSP/tour.h
+++ traveling_sales_person/VS2013/TSP/tour.h
@@ -10,10 +10,22 @@
 #define TSP_TOUR_H
 
 #include <list>
+#include <algorithm>
+#include <iostream>
 
 #include "point.h"
 
+using std::cout;
+using std::endl;
+
 // Typedef for a tour
 typedef std::list<point_t> tour_t;
+
+std::string to_string (tour_t tour)
+	{
+	std::string str;
+	std::for_each (tour.begin (), tour.end (), [&str](point_t p) { str += to_string (p) + "\n"; });
+	return str;
+	}
 
 #endif // TSP_TOUR_H
