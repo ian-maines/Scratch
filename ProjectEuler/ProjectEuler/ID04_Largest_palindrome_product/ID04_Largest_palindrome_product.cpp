c2a7d722a335a1e214d31902327174aa8d55f054
ian-maines <ian-maines@users.noreply.github.com>
2019-01-19 12:08:31 -0500

    Problem 4

---
+++ ProjectEuler/ProjectEuler/ID04_Largest_palindrome_product/ID04_Largest_palindrome_product.cpp
@@ -0,0 +1,23 @@
+// ID04_Largest_palindrome_product.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <cstdint>
+#include <vector>
+
+/* https://projecteuler.net/problem=4
+A palindromic number reads the same both ways.The largest palindrome made from the product of two 2 - digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3 - digit numbers.
+*/
+
+namespace
+	{
+
+	}
+
+int main ()
+	{
+	// Algorithm - O(n^2) loop over all 3-digit numbers. (Unclear from problem statement if that starts at 1 or 100 - hmm. Assume for now means at 100)
+	}
