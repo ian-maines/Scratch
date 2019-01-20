25049b560bb867387f3d30e4691b21a609e0a987
ian-maines <ian-maines@users.noreply.github.com>
2019-01-20 18:01:09 -0500

    Working on 54

--- ProjectEuler/ProjectEuler/ID54_Poker_hands/ID54_Poker_hands.cpp
+++ ProjectEuler/ProjectEuler/ID54_Poker_hands/ID54_Poker_hands.cpp
@@ -105,13 +105,13 @@ namespace
 				return std::string(str);
 				}
 
+			value_t GetValue () const { return m_value; }
+			suit_t GetSuit () const { return m_suit; }
+
 		private:
 			const value_t m_value;
 			const suit_t m_suit;
 		};
-
-	// TODO: Hand valuation.
-	// We need a way to organize the rules for how a hand wins in such a way that judging hands is fairly clear and straightforward
 
 	class CHand
 		{
@@ -135,8 +135,59 @@ namespace
 				return str;
 				}
 
+			const hand_t& get () const { return m_hand; }
+
 		private:
 			hand_t m_hand;
+		};
+
+	// TODO: Hand valuation.
+	// We need a way to organize the rules for how a hand wins in such a way that judging hands is fairly clear and straightforward
+
+	// Take one: start at the highest value hand and work downward:
+	/*
+	High Card : Highest value card.
+	One Pair : Two cards of the same value.
+	Two Pairs : Two different pairs.
+	Three of a Kind : Three cards of the same value.
+	Straight : All cards are consecutive values.
+	Flush : All cards of the same suit.
+	Full House : Three of a kind and a pair.
+	Four of a Kind : Four cards of the same value.
+	Straight Flush : All cards are consecutive values of same suit.
+	Royal Flush : Ten, Jack, Queen, King, Ace, in same suit.
+	*/
+	class CEvaluator
+		{
+		public:
+
+
+		private:
+			bool _IsRoyalFlush (const CHand& hand)
+				{
+				if (_IsFlush (hand))
+					{
+					
+					}
+				return false;
+				}
+
+			bool _IsFlush (const CHand& hand)
+				{
+				bool bFlush = true;
+				const CHand::hand_t& h (hand.get ());
+				suit_t last_suit = h.front().GetSuit();
+				std::for_each (h.begin (), h.end (), [&bFlush, &last_suit](const CCard& card) 
+					{
+					if (card.GetSuit () != last_suit)
+						{
+						bFlush = false;
+						}
+					last_suit = card.GetSuit (); 
+					});
+
+				return bFlush;
+				}
 		};
 
 	class CMatch
