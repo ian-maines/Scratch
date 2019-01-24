879288a4b9bab944ee8aafe7d8f634b684a4f00e
ian-maines <ian-maines@users.noreply.github.com>
2019-01-23 20:44:48 -0500

    Begin modifications to support players tied with same hand type

--- ProjectEuler/ID54_Poker_hands/evaluator.h
+++ ProjectEuler/ID54_Poker_hands/evaluator.h
@@ -28,14 +28,73 @@ class CEvaluator
 	{
 	public:
 		static bool IsRoyalFlush (const CHand& hand);
-		static bool IsStraightFlush (const CHand& hand);
-		static bool Has4OfAKind (const CHand& hand);
-		static bool IsFullHouse (const CHand& hand);
-		static bool IsFlush (const CHand& hand);
-		static bool IsStraight (const CHand& hand);
-		static bool Has3OfAKind (const CHand& hand);
-		static bool HasTwoPair (const CHand& hand);
-		static bool HasPair (const CHand& hand);
+
+		struct str_flush_t
+			{
+			bool bIsStraightFlush = false;
+			value_t high_card = Two;
+			};
+		static const str_flush_t IsStraightFlush (const CHand& hand);
+
+		struct four_oak_t
+			{
+			bool bHas4oak = false;
+			value_t high_card = Two;
+			value_t four_oak_card = Two;
+			};
+		static const four_oak_t Has4OfAKind (const CHand& hand);
+
+		struct full_house_t
+			{
+			bool bIsFullHouse = false;
+			value_t three_value = Two;
+			};
+		static const full_house_t IsFullHouse (const CHand& hand);
+
+		struct flush_t
+			{
+			bool bIsFlush = false;
+			// Highest value card at the end.
+			std::vector<value_t> sorted_cards;
+			};
+		static const flush_t IsFlush (const CHand& hand);
+
+		struct straight_t
+			{
+			bool bIsStraight = false;
+			value_t high_card = Two;
+			};
+		static const straight_t IsStraight (const CHand& hand);
+
+		struct three_oak_t
+			{
+			bool bIs3Oak = false;
+			value_t three_cards_value = Two;
+			// Only the two cards that aren't part of the three of a kind, highest value card at the end.
+			std::vector<value_t> sorted_rem_cards;
+			};
+		static const three_oak_t Has3OfAKind (const CHand& hand);
+
+		struct two_pair_t
+			{
+			bool bIsTwoPair = false;
+			value_t high_pair = Three;
+			value_t low_pair = Two;
+			value_t high_card = Four;
+			};
+		static const two_pair_t HasTwoPair (const CHand& hand);
+
+		struct pair_t
+			{
+			bool bHasPair = false;
+			value_t pair_value = Two;
+			// Only the two cards that aren't part of the pair, highest value card at the end.
+			std::vector<value_t> sorted_rem_cards;
+			};
+		static const pair_t HasPair (const CHand& hand);
+
+		// Returns vector of sorted values with the highest value card at the end.
+		const std::vector<value_t> GetSortedValues (const CHand& hand);
 
 		enum player_t
 			{
@@ -43,6 +102,10 @@ class CEvaluator
 			player_2,
 			};
 		static player_t CompareHands (const CHand& player1, const CHand& player2);
+		
+		// Expects vector size to be equal. Throws exception if not.
+		// Return designates which player has high card. Exception thrown on tie.
+		static player_t PlayerWithHighCard (std::vector<value_t> player1, std::vector<value_t> player2);
 
 	private:
 		static bool _HasXOfAKind (const CHand& hand, int number);
